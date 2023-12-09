4dc510bc2999ebc764209d9d3e1b47e013869c59
Vincent Sparkes <niventc@gmail.com>
2023-12-09 09:25:46 +0000

    day9

---
+++ day9/part2.py
@@ -0,0 +1,28 @@
+
+from typing import List
+
+def get_diffs(sequence: List[int]) -> List[int]:
+    diffs = []
+    for idx in range(1, len(sequence)):
+        diffs.append(sequence[idx] - sequence[idx - 1])
+    return diffs
+
+
+def get_next(sequence: List[int]) -> int:
+    print(sequence)
+    
+    value = sequence[0]
+    if all([x == value for x in sequence]):
+        return value
+    
+    return sequence[0] - get_next(get_diffs(sequence))
+
+
+with open("input.txt") as file:
+    sequences = [list(map(lambda x: int(x), line.rstrip().split(" "))) for line in file.readlines()]
+
+    print(sequences)
+
+    print(sum([get_next(s) for s in sequences]))
+
+# 1637452029
